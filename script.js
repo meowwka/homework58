46f6bb32383b56d006d29735ee928e146432f768
Taazim Temirova <tezi.temirova@gmail.com>
2020-04-05 20:43:01 +0600

    hided splash and no-scroll clases by conditional operator

--- script.js
+++ script.js
@@ -4,21 +4,33 @@
 
   let catPage = document.querySelector(".no-scroll");
   let logIn = document.querySelector(".page-splash");
+    console.log(catPage);
+
 
   // logIn.classList.toggle("page-splash");
 
-  logIn.addEventListener("click", hideSplashScreen);
+  // logIn.addEventListener("click", hideSplashScreen);
+
+  let logHide = document.querySelector(".page-splash").hidden = false;
+  console.log(logHide);
+
+  if(!logHide){
+      logIn.addEventListener("click", hideSplashScreen);
+  } else {
+    logIn.addEventListener("click", showSplashScreen);
+  }
 
 
 function showSplashScreen(){
-  catPage.hidden = true;
-  logIn.hidden = true;
+  logIn.classList.add("page-slash");
+  catPage.classList.add("no-scroll");
+
   console.log(catPage);
 }
 
 function hideSplashScreen(){
-    logIn.classList.toggle("page-splash");
-    catPage.classList.toggle("no-scroll");
+    logIn.classList.remove("page-splash");
+    catPage.classList.remove("no-scroll");
 }
 
 // catPage.addEventListener("click", showSplashScreen);
