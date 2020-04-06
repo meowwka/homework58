63dcb8b83f784dd9218416d205198c0f8b47c6b4
Taazim Temirova <tezi.temirova@gmail.com>
2020-04-06 16:22:46 +0600

    created postElement fuction

--- script.js
+++ script.js
@@ -109,6 +109,49 @@ function createCommentElement(comment){
   return elem;
 }
 
+function createPostElement(post){
+  let elem = document.createElement('div');
+  elem.classList.add('card');
+  elem.classList.add('my-3');
+  elem.innerHTML = 
+  '<div>' +
+    '<img class="d-block w-100" src="' + post.image + '" alt="Post image">' +
+  '</div>' +
+    '<div class="px-4 py-3">' +
+    '<div class="d-flex justify-content-around">' +
+      '<span class="h1 mx-2 text-danger">' +
+      '<i class="fas fa-heart"></i>' +
+      '</span>' +
+      '<span class="h1 mx-2 muted">' +
+      '<i class="far fa-heart"></i>' +
+      '</span>' +
+      '<span class="h1 mx-2 muted">' +
+      '<i class="far fa-comment"></i>' +
+      '</span>' +
+      '<span class="mx-auto"></span>' +
+      '<span class="h1 mx-2 muted">' +
+      '<i class="far fa-bookmark"></i>' +
+      '</span>' +
+      '<span class="h1 mx-2 muted">' +
+      '<i class="fas fa-bookmark"></i>' +
+      '</span>' +
+    '</div>' +
+  '<hr>' +
+  '<div>' +
+    '<p>' + post.descriprtion + '</p>' +
+  '</div>' +
+  '<hr>' +
+  '<div id="comments">' +
+  '<div class="py-2 pl-3">' +
+          '<a href="#" class="muted">' + 'someusername' + '</a>' +
+          '<p>' + 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum ad est cumque nulla voluptatem enim voluptas minima illum quis! Voluptatibus dolorem minus tempore aliquid corrupti nesciunt, obcaecati fuga natus officiis.' + '</p>' +
+        '</div>' +
+  '</div>' + '</div>';
+  return elem;
+}
+
+
+
 
 
 
